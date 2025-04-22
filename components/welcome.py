89cd0c6a157e272218cb2aaf71caec3983d88605
AJ <faronsantoso105@gmail.com>
2025-04-22 20:10:58 +1200

    feat: random bot names to welcome

--- components/welcome.py
+++ components/welcome.py
@@ -1,3 +1,10 @@
-print("============================")
-print("= Welcome to best pie shop =")
-print("============================")
+import random
+
+bot_names = ["Alice", "Bob", "Charlie", "Daisy", "Eve"]
+
+# Randomly select a bot name from the list
+bot_name = random.choice(bot_names)
+
+print("====================================================")
+print(f"= Welcome to best pie shop, my name is {bot_name} =")
+print("====================================================")
