86576e894029af541d835ad96f687d227c42be35
AJ <faronsantoso105@gmail.com>
2025-04-25 22:04:20 +1200

    feat: pickup question is asked again if invalid input

--- components/pickup_method.py
+++ components/pickup_method.py
@@ -1,13 +1,15 @@
-print("Would you like to pick up your order or have it delivered?")
-print("Input 'pickup' or 'p' for Pick up")
-print("Input 'delivery' or 'd' for Delivery")
+while True:
+    print("Would you like to pick up your order or have it delivered?")
+    print("Input 'pickup' or 'p' for Pick up")
+    print("Input 'delivery' or 'd' for Delivery")
 
-choice = input("Enter your choice: ").lower()
+    choice = input("Enter your choice: ").lower()
 
-if choice in ("pickup", "pick up", "pick", "p"):
-    print("You have chosen to pick up your order.")
-
-elif choice in ("delivery", "deliver", "d"):
-    print("You have chosen to have your order delivered.")
-else:
-    print("Invalid choice.")
+    if choice in ("pickup", "pick up", "pick", "p"):
+        print("You have chosen to pick up your order.")
+        break
+    elif choice in ("delivery", "deliver", "d"):
+        print("You have chosen to have your order delivered.")
+        break
+    else:
+        print("Invalid choice.")
