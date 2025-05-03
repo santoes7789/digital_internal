05506c18c6ba842f3a78627e9a9dbb4a86924191
AJ <faronsantoso105@gmail.com>
2025-05-03 22:27:52 +1200

    oops: forgot to uncomment

--- components/customer_details.py
+++ components/customer_details.py
@@ -3,21 +3,21 @@ pickup_method = "delivery"
 
 customer_details = {}
 
-# while True:
-#     user_input = input("Enter your name: ")
-#     if validator.validate_name(user_input):
-#         customer_details["name"] = user_input
-#         break
-#     else:
-#         print("Invalid name. A valid name should not include numbers or special characters.")
+while True:
+    user_input = input("Enter your name: ")
+    if validator.validate_name(user_input):
+        customer_details["name"] = user_input
+        break
+    else:
+        print("Invalid name. A valid name should not include numbers or special characters.")
         
-# while True:
-#     user_input = input("Enter your phone number: ")
-#     if validator.validate_phone(user_input):
-#         customer_details["phone"] = user_input
-#         break
-#     else:
-#         print("Invalid phone number. A phone number should consist of 8-10 digits.")
+while True:
+    user_input = input("Enter your phone number: ")
+    if validator.validate_phone(user_input):
+        customer_details["phone"] = user_input
+        break
+    else:
+        print("Invalid phone number. A phone number should consist of 8-10 digits.")
 
 while True:
     user_input = input("Enter your address: ")
